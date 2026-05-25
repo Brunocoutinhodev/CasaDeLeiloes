306d2992836ebd2860a1a703eb4e210db0c2f784
Bruno Coutinho <Brunocoutinhonegocios@outlook.com>
2026-05-25 06:33:26 -0300

    commit #1: Funcionalidade de vender produto e botao funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,70 +7,82 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-   public void cadastrarProduto (ProdutosDTO produto){
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
         conn = new conectaDAO().connectDB();
-        
-        
+
         String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
-        
+
         try {
             prep = conn.prepareStatement(sql);
-            
+
             prep.setString(1, produto.getNome());
-            prep.setInt(2, produto.getValor()); 
+            prep.setInt(2, produto.getValor());
             prep.setString(3, produto.getStatus());
-            
+
             prep.executeUpdate();
-            
+
             JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
-            
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
         }
     }
-    
-   public ArrayList<ProdutosDTO> listarProdutos(){
-        listagem.clear(); 
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+        listagem.clear();
+
         conn = new conectaDAO().connectDB();
-        
+
         String sql = "SELECT * FROM produtos";
-        
+
         try {
             prep = conn.prepareStatement(sql);
             resultset = prep.executeQuery();
-            
+
             while (resultset.next()) {
                 ProdutosDTO produto = new ProdutosDTO();
                 produto.setId(resultset.getInt("id"));
                 produto.setNome(resultset.getString("nome"));
                 produto.setValor(resultset.getInt("valor"));
                 produto.setStatus(resultset.getString("status"));
-                
+
                 listagem.add(produto);
             }
-            
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
         }
-        
+
         return listagem;
+    }
+
+    public void venderproduto(int id) {
+        conn = new conectaDAO().connectDB();
+        String sql = "UPDATE produtos SET status = 'Vendido' where id = ?";
+        try {
+            prep = conn.prepareStatement(sql);
+
+            prep.setInt(1, id);
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Produto Vendido Com Sucesso");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro Ao Vender Produto" + e.getMessage());
+
+        }
+
     }
 }
--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /*
@@ -136,12 +137,17 @@ public class listagemVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
-        String id = id_produto_venda.getText();
-        
-        ProdutosDAO produtosdao = new ProdutosDAO();
-        
-        //produtosdao.venderProduto(Integer.parseInt(id));
-        listarProdutos();
+     int linhaSelecionada = listaProdutos.getSelectedRow();
+     
+     if(linhaSelecionada== -1){
+         JOptionPane.showMessageDialog(null, "Selecione um Produto na tabela pra vender!");
+}else{
+         int id = Integer.parseInt(listaProdutos.getValueAt(linhaSelecionada,0).toString());
+          ProdutosDAO produtosdao= new ProdutosDAO();
+          produtosdao.venderproduto(id);
+          
+          listarProdutos();
+     }
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
